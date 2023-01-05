a340dd775c999645032246cb0d252748c201a5b3
2jiwon <ljwjulian@gmail.com>
2023-01-05 20:14:28 +0900

    Product 모델과 컨트롤러 추가

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Product::create([
+            'name' => $request->name,
+            'detail' => $request->detail,
+            'stock_amount' => $request->stock_amount,
+            'supply_price' => $request->supply_price,
+            'selling_price' => $request->selling_price,
+            'delivery_fee' => $request->delivery_fee,
+            'category' => json_encode([$request->cate1, $request->cate2, $request->cate3]),
+            'is_selling' => $request->is_selling ?? 'N',
+            'is_displaying' => $request->is_displaying ?? 'N',
+        ]);
+
+        return response()->json(['result' => 'success']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
