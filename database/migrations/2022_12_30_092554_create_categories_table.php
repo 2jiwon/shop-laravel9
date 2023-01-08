c015cb741e9506623f2d3eee61a6c4f50ea2c082
2jiwon <ljwjulian@gmail.com>
2023-01-09 01:04:19 +0900

    categories 테이블 속성 추가 및 수정

--- database/migrations/2022_12_30_092554_create_categories_table.php
+++ database/migrations/2022_12_30_092554_create_categories_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->id();
             $table->string('type', 5)->comment('main, sub1, sub2, sub3');
             $table->string('code', 5)->comment('카테고리 코드');
-            $table->string('parent', 5)->comment('부모 카테고리 코드');
-            $table->integer('order')->comment('표시 순서');
+            $table->string('parent', 5)->nullable()->comment('부모 카테고리 코드');
+            $table->integer('order')->nullable()->comment('표시 순서');
         });
     }
 
