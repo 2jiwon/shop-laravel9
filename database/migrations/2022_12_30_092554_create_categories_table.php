2a63186afbd67c44305310ec171893af79350fce
2jiwon <ljwjulian@gmail.com>
2023-01-09 21:57:20 +0900

    categories 테이블 migration 수정

--- database/migrations/2022_12_30_092554_create_categories_table.php
+++ database/migrations/2022_12_30_092554_create_categories_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('type', 5)->comment('main, sub1, sub2, sub3');
             $table->string('code', 5)->comment('카테고리 코드');
-            $table->string('parent', 5)->nullable()->comment('부모 카테고리 코드');
+            $table->string('parent', 50)->nullable()->comment('부모 카테고리 코드');
             $table->integer('order')->nullable()->comment('표시 순서');
         });
     }
