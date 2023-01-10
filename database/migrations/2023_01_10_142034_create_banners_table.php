6e49b547856ce296c8d38895afef850c9812442c
2jiwon <ljwjulian@gmail.com>
2023-01-10 23:23:59 +0900

    migration 추가 > banners 테이블

---
+++ database/migrations/2023_01_10_142034_create_banners_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banners', function (Blueprint $table) {
+            $table->id();
+            $table->string('type', 10)->comment('main, collection, event');
+            $table->string('filename', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('banners');
+    }
+};
