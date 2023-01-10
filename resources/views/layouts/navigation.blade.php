7bd4700a54bcae88bf422bfa998ac75d035f7798
2jiwon <ljwjulian@gmail.com>
2023-01-10 23:16:44 +0900

    메인 레이아웃 전체적으로 변경

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,100 +1,188 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
-    <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a>
-                </div>
-
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
-            </div>
-
-            <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
-
-                            <div class="ml-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
-
-                    <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
-                        </x-dropdown-link>
-
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
-            </div>
-
-            <!-- Hamburger -->
-            <div class="-mr-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
+<div class="container relative">
+    <div class="relative z-50 py-3 shadow-xs lg:py-6">
+        <div class="flex items-center justify-between">
+            <div class="flex items-center">
+                <button
+                    x-data="{usedKeyboard: false}"
+                    @keydown.window.tab="usedKeyboard = true"
+                    @click="$dispatch('open-menu', { open: true })"
+                    :class="{'focus:outline-none': !usedKeyboard}">
+                    <i class="bx bx-menu text-4xl text-primary"></i>
                 </button>
+                <a
+                href="/search"
+                class="group ml-2 hidden cursor-pointer rounded-full border-2 border-transparent p-2 transition-colors hover:border-primary focus:outline-none sm:ml-3 sm:p-4 md:ml-5 lg:mr-8 lg:block">
+                <img
+                    src="https://elyssi.redpixelthemes.com/assets/img/icons/icon-search.svg"
+                    class="block h-5 w-5 group-hover:hidden sm:h-6 sm:w-6 md:h-8 md:w-8"
+                    alt="icon search"/>
+                <img
+                    src="https://elyssi.redpixelthemes.com/assets/img/icons/icon-search-hover.svg"
+                    class="hidden h-5 w-5 group-hover:block sm:h-6 sm:w-6 md:h-8 md:w-8"
+                    alt="icon search hover"/>
+                </a>
+
+                <button
+                @click="mobileSearch = !mobileSearch"
+                class="group ml-2 block cursor-pointer rounded-full border-2 border-transparent p-2 transition-colors hover:border-primary focus:outline-none sm:ml-3 sm:p-4 md:ml-5 lg:mr-8 lg:hidden">
+                <img
+                    src="https://elyssi.redpixelthemes.com/assets/img/icons/icon-search.svg"
+                    class="block h-5 w-5 group-hover:hidden sm:h-6 sm:w-6 md:h-8 md:w-8"
+                    alt="icon search"/>
+                <img
+                    src="https://elyssi.redpixelthemes.com/assets/img/icons/icon-search-hover.svg"
+                    class="hidden h-5 w-5 group-hover:block sm:h-6 sm:w-6 md:h-8 md:w-8"
+                    alt="icon search hover"/>
+                </button>
+
+                <a
+                href="/account/wishlist"
+                class="group hidden rounded-full border-2 border-transparent p-2 transition-all hover:border-primary sm:p-4 lg:block">
+                <img
+                    src="https://elyssi.redpixelthemes.com/assets/img/icons/icon-heart.svg"
+                    class="block h-5 w-5 group-hover:hidden sm:h-6 sm:w-6 md:h-8 md:w-8"
+                    alt="icon heart"/>
+                <img
+                    src="https://elyssi.redpixelthemes.com/assets/img/icons/icon-heart-hover.svg"
+                    class="hidden h-5 w-5 group-hover:block sm:h-6 sm:w-6 md:h-8 md:w-8"
+                    alt="icon heart hover"/>
+                </a>
             </div>
+
+            <!-- 타이틀 로고 -->
+            <a href="/">
+                <!-- <img src="https://elyssi.redpixelthemes.com/assets/img/logo-elyssi.svg" class="h-auto w-28 sm:w-48" alt="logo"/> -->
+                <div class="text-teal-800 font-extrabold lg:text-4xl">
+                    <h1>FANCYTANK SHOP</h1>
+                </div>
+            </a>
+            
+            <div class="flex items-center">
+                <a
+                href="/account/dashboard"
+                class="group rounded-full border-2 border-transparent p-2 transition-all hover:border-primary sm:p-4">
+                <img
+                    src="https://elyssi.redpixelthemes.com/assets/img/icons/icon-user.svg"
+                    class="block h-5 w-5 group-hover:hidden sm:h-6 sm:w-6 md:h-8 md:w-8"
+                    alt="icon user"/>
+                <img
+                    src="https://elyssi.redpixelthemes.com/assets/img/icons/icon-user-hover.svg"
+                    class="hidden h-5 w-5 group-hover:block sm:h-6 sm:w-6 md:h-8 md:w-8"
+                    alt="icon user hover"/>
+                </a>
+
+                <a
+                href="/cart/index"
+                class="group ml-2 hidden rounded-full border-2 border-transparent p-2 transition-all hover:border-primary sm:ml-3 sm:p-4 md:ml-5 lg:ml-8 lg:block">
+                <img
+                    src="https://elyssi.redpixelthemes.com/assets/img/icons/icon-cart.svg"
+                    class="block h-5 w-5 group-hover:hidden sm:h-6 sm:w-6 md:h-8 md:w-8"
+                    alt="icon cart"/>
+                <img
+                    src="https://elyssi.redpixelthemes.com/assets/img/icons/icon-cart-hover.svg"
+                    class="hidden h-5 w-5 group-hover:block sm:h-6 sm:w-6 md:h-8 md:w-8"
+                    alt="icon cart hover"/>
+                </a>
+
+                <span
+                @click="mobileCart = !mobileCart"
+                class="group ml-2 block rounded-full border-2 border-transparent p-2 transition-all hover:border-primary sm:ml-3 sm:p-4 md:ml-5 lg:ml-8 lg:hidden">
+                <img
+                    src="https://elyssi.redpixelthemes.com/assets/img/icons/icon-cart.svg"
+                    class="block h-5 w-5 group-hover:hidden sm:h-6 sm:w-6 md:h-8 md:w-8"
+                    alt="icon cart"/>
+                <img
+                    src="https://elyssi.redpixelthemes.com/assets/img/icons/icon-cart-hover.svg"
+                    class="hidden h-5 w-5 group-hover:block sm:h-6 sm:w-6 md:h-8 md:w-8"
+                    alt="icon cart hover"/>
+                </span>
+            </div>
+        
         </div>
     </div>
+</div>
+<!-- 로고 & 메뉴 End -->
 
-    <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
+<section
+	x-data="slideout()"
+	x-cloak
+	@open-menu.window="open = $event.detail.open"
+	@keydown.window.tab="usedKeyboard = true"
+	@keydown.escape="open = false"
+	x-init="init()">
+	<div
+		x-show.transition.opacity.duration.500="open"
+		@click="open = false"
+		class="fixed inset-0 bg-black bg-opacity-25"></div>
+	<div
+		class="fixed transition duration-300 left-0 top-0 transform w-full max-w-xs h-screen bg-gray-100 overflow-hidden z-50"
+		:class="{'-translate-x-full': !open}">
+		<button
+			@click="open = false"
+			x-ref="closeButton"
+			:class="{'focus:outline-none': !usedKeyboard}"
+			class="fixed top-0 right-0 mr-0 mt-2 z-50">
+            <i class="bx bx-x mr-6 cursor-pointer text-2xl text-grey-darkest sm:text-3xl"></i>
+        </button>
+		<div class="p-16 px-6 absolute top-0 w-full h-full overflow-y-scroll">
+            <div class="container text-grey mx-auto px-2 py-2" x-data="{
+                categories: [
+                    {
+                        main: '여성복',
+                        sub: '상의',
+                        isOpen: true,
+                    },
+                    {
+                        main: '남성복',
+                        sub: '와이셔츠',
+                        isOpen: false,
+                    },
+                    {
+                        main: '키즈',
+                        sub: '티셔츠',
+                        isOpen: false,
+                    },
+                ]
+            }">
+            <h2 class="text-2xl font-bold">메뉴</h2>
+            <div class="leading-loose text-lg mt-6">
+                <template x-for="(category, index) in categories" :key="category.main">
+                <div>
+                    <button class="w-full font-bold py-3 flex justify-between items-center mt-4" :class="index !== categories.length - 1 && 'border-b border-gray-400'" @click="categories = categories.map(f => ({ ...f, isOpen: f.main !== category.main ? false : !f.isOpen}))">
+                    <!-- Specs has it that only one component can be open at a time and also you should be able to toggle the open state of the active component too -->
+                    <div x-text="category.main"></div>
+                    <svg x-show="!category.isOpen" class="fill-current" viewBox="0 0 24 24" width="24" height="24">
+                        <path class="heroicon-ui" d="M12 22a10 10 0 110-20 10 10 0 010 20zm0-2a8 8 0 100-16 8 8 0 000 16zm1-9h2a1 1 0 010 2h-2v2a1 1 0 01-2 0v-2H9a1 1 0 010-2h2V9a1 1 0 012 0v2z" />
+                    </svg>
+                    <svg x-show="category.isOpen" class="fill-current" viewBox="0 0 24 24" width="24" height="24">
+                        <path class="heroicon-ui" d="M12 22a10 10 0 110-20 10 10 0 010 20zm0-2a8 8 0 100-16 8 8 0 000 16zm4-8a1 1 0 01-1 1H9a1 1 0 010-2h6a1 1 0 011 1z" />
+                    </svg>
+                    </button>
 
-        <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
+                    <div class="text-gray-700 text-sm mt-2" x-text="category.sub" x-show="category.isOpen"></div>
+                </div>
+                </template>
             </div>
-
-            <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
-                </x-responsive-nav-link>
-
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
             </div>
         </div>
-    </div>
-</nav>
+	</div>
+</section>
+
+<script>
+function slideout() {
+	return {
+		open: false,
+		usedKeyboard: false,
+		init() {
+			this.$watch('open', value => {
+				value && this.$refs.closeButton.focus()
+				this.toggleOverlay()
+			})
+			this.toggleOverlay()
+		},
+		toggleOverlay() {
+			document.body.classList[this.open ? 'add' : 'remove']('h-screen', 'overflow-hidden')
+		}
+	}
+}
+</script>
