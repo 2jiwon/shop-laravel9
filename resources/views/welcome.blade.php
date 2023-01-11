b3148f1007725c8b7e18dd8e4d7a37143a02ff69
2jiwon <ljwjulian@gmail.com>
2023-01-11 23:48:43 +0900

    일단 예전 welcome blade도 보존

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,6 +82,7 @@
             </label>
             <input class="hidden" type="checkbox" id="menu-toggle" />
 
+            <!-- 왼쪽 메뉴 -->
             <div class="hidden md:flex md:items-center md:w-auto w-full order-3 md:order-1" id="menu">
                 <nav>
                     <ul class="md:flex items-center justify-between text-base text-gray-700 pt-4 md:pt-0">
@@ -91,6 +92,7 @@
                 </nav>
             </div>
 
+            <!-- shop title -->
             <div class="order-1 md:order-2">
                 <a class="flex items-center tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl " href="#">
                     <svg class="fill-current text-gray-800 mr-2" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
@@ -100,16 +102,17 @@
                 </a>
             </div>
 
+            <!-- 오른쪽 메뉴 -->
             <div class="order-2 md:order-3 flex items-center" id="nav-content">
 
-                <a class="inline-block no-underline hover:text-black" href="/login">
+                <a class="inline-block no-underline hover:text-black" href="/profile">
                     <svg class="fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                         <circle fill="none" cx="12" cy="7" r="3" />
                         <path d="M12 2C9.243 2 7 4.243 7 7s2.243 5 5 5 5-2.243 5-5S14.757 2 12 2zM12 10c-1.654 0-3-1.346-3-3s1.346-3 3-3 3 1.346 3 3S13.654 10 12 10zM21 21v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h2v-1c0-2.757 2.243-5 5-5h4c2.757 0 5 2.243 5 5v1H21z" />
                     </svg>
                 </a>
 
-                <a class="pl-3 inline-block no-underline hover:text-black" href="#">
+                <a class="pl-3 inline-block no-underline hover:text-black" href="/cart">
                     <svg class="fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                         <path d="M21,7H7.462L5.91,3.586C5.748,3.229,5.392,3,5,3H2v2h2.356L9.09,15.414C9.252,15.771,9.608,16,10,16h8 c0.4,0,0.762-0.238,0.919-0.606l3-7c0.133-0.309,0.101-0.663-0.084-0.944C21.649,7.169,21.336,7,21,7z M17.341,14h-6.697L8.371,9 h11.112L17.341,14z" />
                         <circle cx="10.5" cy="18.5" r="1.5" />
@@ -121,6 +124,7 @@
         </div>
     </nav>
 
+    <!-- main carousel -->
     <div class="carousel relative container mx-auto" style="max-width:1600px;">
         <div class="carousel-inner relative overflow-hidden w-full">
             <!--Slide 1-->
@@ -195,21 +199,17 @@
 Alternatively if you want to just have a single hero
 
 <section class="w-full mx-auto bg-nordic-gray-light flex pt-12 md:pt-0 md:items-center bg-cover bg-right" style="max-width:1600px; height: 32rem; background-image: url('https://images.unsplash.com/photo-1422190441165-ec2956dc9ecc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80');">
-
 	<div class="container mx-auto">
-
 		<div class="flex flex-col w-full lg:w-1/2 justify-center items-start  px-6 tracking-wide">
 			<h1 class="text-black text-2xl my-4">Stripy Zig Zag Jigsaw Pillow and Duvet Set</h1>
 			<a class="text-xl inline-block no-underline border-b border-gray-600 leading-relaxed hover:text-black hover:border-black" href="#">products</a>
-
 		</div>
-
 	  </div>
-
 </section>
 
 -->
-
+    
+    <!-- 상품 목록 -->
     <section class="bg-white py-8">
 
         <div class="container mx-auto flex items-center flex-wrap pt-4 pb-12">
@@ -349,38 +349,69 @@ Alternatively if you want to just have a single hero
 
     <section class="bg-white py-8">
 
+        <div class="flex items-center justify-center pt-5">
+          
+          <a href="https://www.google.com" class="group">
+            <div class="mr-5 flex items-center rounded-full bg-white px-2 py-2 transition-colors group-hover:bg-primary">
+              <i class="bx bxl-facebook text-secondary transition-colors group-hover:text-white"></i>
+            </div>
+          </a>
+          
+          <a href="https://www.google.com" class="group">
+            <div class="mr-5 flex items-center rounded-full bg-white px-2 py-2 transition-colors group-hover:bg-primary">
+              <i class="bx bxl-twitter text-secondary transition-colors group-hover:text-white"></i>
+            </div>
+          </a>
+          
+          <a href="https://www.google.com" class="group">
+            <div class="mr-5 flex items-center rounded-full bg-white px-2 py-2 transition-colors group-hover:bg-primary">
+              <i class="bx bxl-instagram text-secondary transition-colors group-hover:text-white"></i>
+            </div>
+          </a>
+          
+          <a href="https://www.google.com" class="group">
+            <div class="mr-5 flex items-center rounded-full bg-white px-2 py-2 transition-colors group-hover:bg-primary">
+              <i class="bx bxl-pinterest text-secondary transition-colors group-hover:text-white"></i>
+            </div>
+          </a>
+          
+        </div>
+
         <div class="container py-8 px-6 mx-auto">
-
             <a class="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl mb-8" href="#">
-			About
-		</a>
+			인스타그램
+		    </a>
 
-            <p class="mt-8 mb-8">This template is inspired by the stunning nordic minamalist design - in particular:
-                <br>
-                <a class="text-gray-800 underline hover:text-gray-900" href="http://savoy.nordicmade.com/" target="_blank">Savoy Theme</a> created by <a class="text-gray-800 underline hover:text-gray-900" href="https://nordicmade.com/">https://nordicmade.com/</a> and <a class="text-gray-800 underline hover:text-gray-900" href="https://www.metricdesign.no/" target="_blank">https://www.metricdesign.no/</a></p>
-
-            <p class="mb-8">Lorem ipsum dolor sit amet, consectetur <a href="#">random link</a> adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel risus commodo viverra maecenas accumsan lacus vel facilisis volutpat. Vitae aliquet nec ullamcorper sit. Nullam eget felis eget nunc lobortis mattis aliquam. In est ante in nibh mauris. Egestas congue quisque egestas diam in. Facilisi nullam vehicula ipsum a arcu. Nec nam aliquam sem et tortor consequat. Eget mi proin sed libero enim sed faucibus turpis in. Hac habitasse platea dictumst quisque. In aliquam sem fringilla ut. Gravida rutrum quisque non tellus orci ac auctor augue mauris. Accumsan lacus vel facilisis volutpat est velit egestas dui id. At tempor commodo ullamcorper a. Volutpat commodo sed egestas egestas fringilla. Vitae congue eu consequat ac.</p>
-
+            <p class="mt-8 mb-8">something</p>
+            <p class="mb-8">comment</p>
         </div>
 
     </section>
 
+    <!-- 하단 -->
     <footer class="container mx-auto bg-white py-8 border-t border-gray-400">
         <div class="container flex px-3 py-8 ">
             <div class="w-full mx-auto flex flex-wrap">
-                <div class="flex w-full lg:w-1/2 ">
+                <div class="flex w-full lg:w-1/2">
                     <div class="px-3 md:px-0">
-                        <h3 class="font-bold text-gray-900">About</h3>
+                        <h3 class="font-bold text-gray-900">고객센터</h3>
                         <p class="py-4">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel mi ut felis tempus commodo nec id erat. Suspendisse consectetur dapibus velit ut lacinia.
+                            1588-0000
                         </p>
                     </div>
+                    <div class="px-3 md:px-0">
+                        
+                    </div>
                 </div>
+                
                 <div class="flex w-full lg:w-1/2 lg:justify-end lg:text-right">
                     <div class="px-3 md:px-0">
                         <h3 class="font-bold text-gray-900">Social</h3>
                         <ul class="list-reset items-center pt-3">
                             <li>
+                                <a class="inline-block no-underline hover:text-black hover:underline py-1" href="#">Add social links</a>
+                                <a class="inline-block no-underline hover:text-black hover:underline py-1" href="#">Add social links</a>
+                                <a class="inline-block no-underline hover:text-black hover:underline py-1" href="#">Add social links</a>
                                 <a class="inline-block no-underline hover:text-black hover:underline py-1" href="#">Add social links</a>
                             </li>
                         </ul>
