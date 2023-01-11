b16b6ab190a4c9709c3531f167df50853360d899
2jiwon <ljwjulian@gmail.com>
2023-01-11 23:50:21 +0900

    head, foot 부분 레이아웃에서 분리

---
+++ resources/views/layouts/foot.blade.php
@@ -0,0 +1,118 @@
+<!-- 하단 링크, 인스타       -->
+      <div class="bg-teal-800">
+        <div class="container py-16 sm:py-20 md:py-24">
+          <div
+            class="mx-auto flex w-5/6 flex-col items-center justify-between lg:flex-row">
+            <div class="text-center lg:text-left">
+              <!-- <h4 class="pb-8 font-dohyeon text-xl font-bold text-white">고객센터</h4> -->
+              <ul class="list-reset">
+                
+                <li class="block pb-2">
+                  <a
+                    href="mailto:shop@fancytank.com"
+                    class=" text-base tracking-wide text-white transition-colors hover:text-primary">shop@fancytank.com</a>
+                </li>
+                
+                <li class="block pb-2">
+                  <a
+                    href=""
+                    class=" text-base tracking-wide text-white transition-colors hover:text-primary">사업자 등록번호 0123 234 222</a>
+                </li>
+                
+                <li class="block pb-2">
+                  <a
+                    href="https://fancytank.com"
+                    class=" text-base tracking-wide text-white transition-colors hover:text-primary">대표자명</a>
+                </li>
+                
+              </ul>
+            </div>
+            <div class="py-16 text-center lg:py-0">
+              <a
+                href="/"
+                class="font-gowunbatang text-4xl uppercase tracking-wider text-white">FANCYTANK</a>
+              <div class="flex items-center justify-center pt-5">
+                
+                <a href="https://fancytank.com" class="group">
+                  <div
+                    class="mr-5 flex items-center rounded-full bg-white px-2 py-2 transition-colors group-hover:bg-primary">
+                    <i
+                      class="bx bxl-facebook text-secondary transition-colors group-hover:text-white"></i>
+                  </div>
+                </a>
+                
+                <a href="https://fancytank.com" class="group">
+                  <div
+                    class="mr-5 flex items-center rounded-full bg-white px-2 py-2 transition-colors group-hover:bg-primary">
+                    <i
+                      class="bx bxl-twitter text-secondary transition-colors group-hover:text-white"></i>
+                  </div>
+                </a>
+                
+                <a href="https://fancytank.com" class="group">
+                  <div
+                    class="mr-5 flex items-center rounded-full bg-white px-2 py-2 transition-colors group-hover:bg-primary">
+                    <i
+                      class="bx bxl-instagram text-secondary transition-colors group-hover:text-white"></i>
+                  </div>
+                </a>
+                
+                <a href="https://fancytank.com" class="group">
+                  <div
+                    class="mr-5 flex items-center rounded-full bg-white px-2 py-2 transition-colors group-hover:bg-primary">
+                    <i
+                      class="bx bxl-pinterest text-secondary transition-colors group-hover:text-white"></i>
+                  </div>
+                </a>
+                
+              </div>
+            </div>
+            <div class="text-center lg:text-left">
+              <!-- <h4 class="pb-8 font-dohyeon text-xl font-bold text-white">Link</h4> -->
+              <ul class="list-reset">
+                <li class="block pb-2">
+                  <a
+                    href="/collection-list"
+                    class=" text-base tracking-wide text-white transition-colors hover:text-primary">이용가이드</a>
+                </li>
+                <li class="block pb-2">
+                  <a
+                    href="/contact"
+                    class=" text-base tracking-wide text-white transition-colors hover:text-primary">이용약관</a>
+                </li>
+                <li class="block pb-2">
+                  <a
+                    href="/single"
+                    class=" text-base tracking-wide text-white transition-colors hover:text-primary">개인정보처리방침</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- 하단 링크, 인스타 End   -->
+
+      <!-- 최하단 copyright -->
+      <div class="container py-4">
+        <p class="text-center font-dohyeon text-base text-secondary">
+          All rights reserved © 2022. 
+          <a href="/admin" target="_blank" class="text-primary">fancytank</a>.
+        </p>
+      </div>
+      <!-- 최하단 copyright End -->
+
+    </div>
+    
+    <script
+      src="https://cdn.jsdelivr.net/npm/@splidejs/splide@3.6.12/dist/js/splide.min.js"
+      crossorigin="anonymous"></script>
+
+    <script src="/assets/theme/js/main.js"></script>
+    
+    <script
+      src="/assets/theme/js/script.js"
+      data-site="NUAESTQS"
+      defer></script>
+    
+  </body>
+</html>
---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ko">
+  <head>
+  <meta charset="utf-8"/>
+  <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+  <title>Home | FANCYTANK SHOP</title>
+
+  <meta property="og:title" content="Home | FANCYTANK SHOP"/>
+  <meta property="og:locale" content="en_US"/>
+  <meta name="theme-color" content="#118ab2"/>
+  <meta property="og:url" content="https://shop.fancytank.com/"/>
+  <meta name="description" content=""/>
+  <meta property="og:site_name" content="FANCYTANK SHOP"/>
+  <meta property="og:image" content=""/>
+
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css"
+    integrity="sha256-imWjOiEEAcjWdL1+inhBu1dWYFyXuiO9vpJVEQd3y/c="
+    crossorigin="anonymous"/>
+
+  <link
+    rel="stylesheet"
+    href="/assets/theme/styles/main.min.css"
+    media="screen"
+    crossorigin="anonymous"/>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/@splidejs/splide@3.6.12/dist/css/splide.min.css"/>
+ 
+  @vite(['resources/css/app.css'])
+
+  <link rel="stylesheet" href="{{ asset('assets/theme/styles/custom.css') }}"/>
+
+  <script src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
+
+  <style>
+    	[x-cloak] { display: none; }
+  </style>
+</head>
