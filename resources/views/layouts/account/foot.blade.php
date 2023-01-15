c42ef973f3e49a1364acc57a89dc339d8db35187
2jiwon <ljwjulian@gmail.com>
2023-01-15 18:12:30 +0900

    account view > 공통 foot 추가

---
+++ resources/views/layouts/account/foot.blade.php
@@ -0,0 +1,9 @@
+        <!-- md 이하 화면에서만 표시되는 로그아웃 버튼 -->
+        <div class="text-right">
+            <a href="/logout" class="lg:w-1/4 mt-24 inline-block md:hidden rounded border border-primary px-8 py-3 font-nanumgothic font-bold text-[#118ab2] transition-all hover:bg-primary hover:text-white">로그아웃</a>
+        </div>
+    
+    </div>
+</div>
+
+@include('layouts.foot')
