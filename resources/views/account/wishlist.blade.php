d3474fe6fcd79179156269d408d75d33fe5c39d5
2jiwon <ljwjulian@gmail.com>
2023-01-15 18:29:28 +0900

    account view > 위시리스트 view 수정

--- resources/views/account/wishlist.blade.php
+++ resources/views/account/wishlist.blade.php
@@ -14,20 +14,20 @@
               <span class="font-hkbold text-sm uppercase text-secondary">상품명</span>
             </div>
             <div class="w-1/4 text-center xl:w-1/5">
-              <span class="font-hkbold text-sm uppercase text-secondary">Quantity</span>
+              <span class="font-hkbold text-sm uppercase text-secondary">수량</span>
             </div>
             <div class="mr-3 w-1/6 text-center md:w-1/5">
-              <span class="font-hkbold text-sm uppercase text-secondary">Price</span>
+              <span class="font-hkbold text-sm uppercase text-secondary">가격</span>
             </div>
             <div class="w-3/10 text-center md:w-1/5">
-              <span class="font-hkbold pr-8 text-sm uppercase text-secondary md:pr-16 xl:pr-8">Action</span>
+              <span class="font-hkbold pr-8 text-sm uppercase text-secondary md:pr-16 xl:pr-8">상태</span>
             </div>
           </div>
         </div>
         
         <div class="mb-3 flex flex-col items-center justify-between rounded bg-white px-4 py-5 shadow sm:flex-row sm:py-4">
           <div class="flex w-full flex-col border-b border-grey-dark pb-4 text-center sm:w-1/3 sm:border-b-0 sm:pb-0 sm:text-left md:w-2/5 md:flex-row md:items-center">
-            <span class="font-hkbold block pb-2 text-center text-sm uppercase text-secondary sm:hidden">Product Name</span>
+            <span class="font-hkbold block pb-2 text-center text-sm uppercase text-secondary sm:hidden">상품명</span>
             <div class="relative mx-auto w-20 sm:mx-0 sm:mr-3 sm:pr-0">
               <div class="aspect-w-1 aspect-h-1 w-full">
                 <img
@@ -39,11 +39,11 @@
             <span class="mt-2 font-hk text-base text-secondary">Classic Beige</span>
           </div>
           <div class="w-full border-b border-grey-dark pb-4 text-center sm:w-1/6 sm:border-b-0 sm:pb-0 md:w-1/6 xl:w-1/5">
-            <span class="font-hkbold block pt-3 pb-2 text-center text-sm uppercase text-secondary sm:hidden">Quantity</span>
+            <span class="font-hkbold block pt-3 pb-2 text-center text-sm uppercase text-secondary sm:hidden">수량</span>
             <span class="font-hk text-secondary">11</span>
           </div>
           <div class="w-full pb-4 text-center sm:w-1/6 sm:pr-6 sm:pb-0 sm:text-right xl:w-1/5 xl:pr-16">
-            <span class="font-hkbold block pt-3 pb-2 text-center text-sm uppercase text-secondary sm:hidden">Price</span>
+            <span class="font-hkbold block pt-3 pb-2 text-center text-sm uppercase text-secondary sm:hidden">가격</span>
             <span class="font-hk text-secondary">$1045</span>
           </div>
           <a href="/" class="btn btn-primary whitespace-nowrap">Order Now</a>
@@ -51,7 +51,7 @@
         
         <div class="mb-3 flex flex-col items-center justify-between rounded bg-white px-4 py-5 shadow sm:flex-row sm:py-4">
           <div class="flex w-full flex-col border-b border-grey-dark pb-4 text-center sm:w-1/3 sm:border-b-0 sm:pb-0 sm:text-left md:w-2/5 md:flex-row md:items-center">
-            <span class="font-hkbold block pb-2 text-center text-sm uppercase text-secondary sm:hidden">Product Name</span>
+            <span class="font-hkbold block pb-2 text-center text-sm uppercase text-secondary sm:hidden">상품명</span>
             <div class="relative mx-auto w-20 sm:mx-0 sm:mr-3 sm:pr-0">
               <div class="aspect-w-1 aspect-h-1 w-full">
                 <img
@@ -63,18 +63,18 @@
             <span class="mt-2 font-hk text-base text-secondary">Party Blake</span>
           </div>
           <div class="w-full border-b border-grey-dark pb-4 text-center sm:w-1/6 sm:border-b-0 sm:pb-0 md:w-1/6 xl:w-1/5">
-            <span class="font-hkbold block pt-3 pb-2 text-center text-sm uppercase text-secondary sm:hidden">Quantity</span>
+            <span class="font-hkbold block pt-3 pb-2 text-center text-sm uppercase text-secondary sm:hidden">수량</span>
             <span class="font-hk text-secondary">10</span>
           </div>
           <div class="w-full pb-4 text-center sm:w-1/6 sm:pr-6 sm:pb-0 sm:text-right xl:w-1/5 xl:pr-16">
-            <span class="font-hkbold block pt-3 pb-2 text-center text-sm uppercase text-secondary sm:hidden">Price</span>
+            <span class="font-hkbold block pt-3 pb-2 text-center text-sm uppercase text-secondary sm:hidden">가격</span>
             <span class="font-hk text-secondary">$1045</span>
           </div>
           <a href="/" class="btn btn-primary whitespace-nowrap">Order Now</a>
         </div>
         
         
-         <!-- pagination -->
+        <!-- pagination -->
         <div class="flex justify-center pt-6 md:justify-end">
           <span class="cursor-pointer pr-5 font-nanumgothic font-semibold text-grey-darkest transition-colors hover:text-black">이전</span>
           <span class="mr-3 flex h-6 w-6 cursor-pointer items-center justify-center rounded-full font-gowunbatang text-sm font-semibold text-black transition-colors hover:bg-primary hover:text-white">1</span>
