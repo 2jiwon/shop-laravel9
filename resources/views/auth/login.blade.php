a60e5cc04527814b3b11e924f48639244943c2ee
2jiwon <ljwjulian@gmail.com>
2023-01-25 15:15:33 +0900

    기존 login ui를 살짝 수정

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,14 +7,14 @@
 
         <!-- Uid -->
         <div>
-            <x-input-label for="uid" :value="__('Id')" />
+            <x-input-label for="uid" :value="__('아이디')" />
             <x-text-input id="uid" class="block mt-1 w-full" type="text" name="uid" :value="old('uid')" required autofocus />
             <x-input-error :messages="$errors->get('uid')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('비밀번호')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -28,20 +28,24 @@
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                <span class="ml-2 text-sm text-gray-600">{{ __('저장하기') }}</span>
             </label>
+        </div>
+        
+        <div class="flex items-center justify-start mt-4">
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    {{ __('비밀번호를 잊어버리셨나요?') }}
+                </a>
+            @endif
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
             <x-primary-button class="ml-3">
-                {{ __('Log in') }}
+                {{ __('로그인') }}
             </x-primary-button>
         </div>
+
+
     </form>
 </x-guest-layout>
