f7c73fd404c3a699270ca08744e78967e9928ace
2jiwon <ljwjulian@gmail.com>
2023-01-25 14:24:35 +0900

    admins table migration 파일 생성

---
+++ database/migrations/2023_01_25_142416_create_admins_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->string('uid', 20)->unique()->comment('아이디');
+            $table->string('name', 10)->nullable()->comment('사용자명');
+            $table->string('email', 30)->unique();
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+         Schema::dropIfExists('admin');
+    }
+};
