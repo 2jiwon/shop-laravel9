a99fe5b23375e9a8c45a54241e58a5d3de1dff8c
2jiwon <ljwjulian@gmail.com>
2023-01-27 14:06:48 +0900

    categories 테이블 컬럼 수정

--- database/migrations/2022_12_30_092554_create_categories_table.php
+++ database/migrations/2022_12_30_092554_create_categories_table.php
@@ -15,9 +15,10 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('type', 5)->comment('main, sub1, sub2, sub3');
-            $table->string('code', 5)->comment('카테고리 코드');
-            $table->string('parent', 50)->nullable()->comment('부모 카테고리 코드');
+            $table->string('type', 5)->comment('main, sub1, sub2');
+            // $table->string('code', 5)->comment('카테고리 코드');
+            $table->string('parent1', 5)->nullable()->comment('상위 카테고리 코드1');
+            $table->string('parent2', 5)->nullable()->comment('상위 카테고리 코드2');
             $table->integer('order')->nullable()->comment('표시 순서');
         });
     }
