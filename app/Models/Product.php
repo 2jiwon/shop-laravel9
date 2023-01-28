fc71a81f14d2fb1013ce23c75c8f5ce7b62c1f0d
2jiwon <ljwjulian@gmail.com>
2023-01-28 16:27:15 +0900

    product 컨트롤러에서 카테고리 정보 가져오는 부분을 모델로 이동

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -19,10 +19,40 @@ class Product extends Model
     }
 
     /**
-     *  Category
+     *  Category를 type에 따라서 상위 Category까지 이름만 배열로 반환
      */
-    public function categories() {
-        return $this->hasMany(Category::class);
+    public static function getCategories($product) {
+        // return $this->hasMany(Category::class, 'id', 'category');
+        $category = [];
+        $data = Category::find($product->category);
+
+        foreach (Category::all() as $cate) {
+            if ($data->type == 'main') {
+                array_push($category, $data->name);
+                break;
+            } else if ($data->type == 'sub1') {
+                if ($cate->id == $data->parent1) {
+                    array_push($category, $cate->name);
+                    array_push($category, $data->name);
+                    break;
+                }
+                continue;
+                
+            } else if ($data->type == 'sub2') {
+                if ($cate->id == $data->parent1) {
+                    array_push($category, $cate->name);
+                    continue;
+                }
+                if ($cate->id == $data->parent2) {
+                    array_push($category, $cate->name);
+                    array_push($category, $data->name);
+                    break;
+                }
+                continue;
+            }
+        }
+
+        return $category;
     }
 
     /**
