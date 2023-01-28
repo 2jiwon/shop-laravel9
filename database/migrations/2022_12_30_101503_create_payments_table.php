0553f9b539e7b707ae705158b6e459e2b70304bb
2jiwon <ljwjulian@gmail.com>
2023-01-28 20:08:14 +0900

    payments 테이블 컬럼 수정

--- database/migrations/2022_12_30_101503_create_payments_table.php
+++ database/migrations/2022_12_30_101503_create_payments_table.php
@@ -18,8 +18,8 @@ return new class extends Migration
             $table->unsignedBigInteger('order_id');
             $table->integer('pay_type')->comment('1:무통장입금, 2:신용,체크카드, 3:기타');
             $table->integer('status')->comment('0:결제취소, 1:입금완료, 2:결제완료, 3:환불요청');
-            $table->string('detail', 30)->comment('결제 정보');
-            $table->string('receipt', 30)->comment('영수증 정보');
+            $table->string('detail', 300)->comment('결제 정보');
+            $table->string('receipt', 300)->comment('영수증 정보');
             $table->unsignedBigInteger('total_amount')->comment('총 결제금액');
             $table->timestamps();
         });
