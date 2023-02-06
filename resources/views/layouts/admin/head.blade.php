a39c3c6d122c401eb6a99f74e4687f4292dab90b
2jiwon <ljwjulian@gmail.com>
2023-02-06 23:37:10 +0900

    admin > head > tinymce init 부분 추가

--- resources/views/layouts/admin/head.blade.php
+++ resources/views/layouts/admin/head.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'ADMIN') }}</title>
 
     <meta name="description" content="">
 
@@ -16,14 +16,20 @@
     <!-- custom css -->
     <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}"/>
 
-     <!-- Tinymce -->
-     <script src="https://cdn.tiny.cloud/1/ttm2ripndo2cw7gbv5crh3gxgykustof3wq45gb3rsmx885j/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
-        <script>
-        tinymce.init({
-            selector:'#detail',
-            height: 300, 
-            menubar: 'edit format'
-        });
-        </script>
+    <!-- Tinymce -->
+    <script src="https://cdn.tiny.cloud/1/ttm2ripndo2cw7gbv5crh3gxgykustof3wq45gb3rsmx885j/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+    <script>
+    tinymce.init({
+        selector: '#detail',
+        height: 300, 
+        menubar: 'edit format'
+    });
+
+    tinymce.init({
+        selector: '#edit_detail',
+        height: 300, 
+        menubar: 'edit format'
+    });
+    </script>
 </head>
 <body class="bg-gray-100 flex">
