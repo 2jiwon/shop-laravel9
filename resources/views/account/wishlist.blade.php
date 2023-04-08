d969730286b5ed919b1cb601ca5e4a4732c09e3c
2jiwon <ljwjulian@gmail.com>
2023-04-08 21:49:37 +0900

    위시리스트 목록 표시 완료

--- resources/views/account/wishlist.blade.php
+++ resources/views/account/wishlist.blade.php
@@ -13,77 +13,63 @@
             <div class="w-1/3 pl-4 md:w-2/5">
               <span class="font-hkbold text-sm uppercase text-secondary">상품명</span>
             </div>
-            <div class="w-1/4 text-center xl:w-1/5">
-              <span class="font-hkbold text-sm uppercase text-secondary">수량</span>
-            </div>
             <div class="mr-3 w-1/6 text-center md:w-1/5">
               <span class="font-hkbold text-sm uppercase text-secondary">가격</span>
             </div>
-            <div class="w-3/10 text-center md:w-1/5">
-              <span class="font-hkbold pr-8 text-sm uppercase text-secondary md:pr-16 xl:pr-8">상태</span>
+            <div class="w-3/10 text-center md:w-1/12">
+              <span class="font-hkbold pr-8 text-sm uppercase text-secondary md:pr-16 xl:pr-8">액션</span>
+            </div>
+            <div class="w-3/10 text-center md:w-1/12">
+              <span class="font-hkbold pr-8 text-sm uppercase text-secondary md:pr-16 xl:pr-8">삭제</span>
             </div>
           </div>
         </div>
         
-        <div class="mb-3 flex flex-col items-center justify-between rounded bg-white px-4 py-5 shadow sm:flex-row sm:py-4">
-          <div class="flex w-full flex-col border-b border-grey-dark pb-4 text-center sm:w-1/3 sm:border-b-0 sm:pb-0 sm:text-left md:w-2/5 md:flex-row md:items-center">
-            <span class="font-hkbold block pb-2 text-center text-sm uppercase text-secondary sm:hidden">상품명</span>
-            <div class="relative mx-auto w-20 sm:mx-0 sm:mr-3 sm:pr-0">
-              <div class="aspect-w-1 aspect-h-1 w-full">
-                <img
-                  src="https://elyssi.redpixelthemes.com/assets/img/unlicensed/shoes-3.png"
-                  alt="product image"
-                  class="object-cover"/>
+        @php
+          $perPage = 3;
+          $currentPage = empty(request()->page) ? 1 : request()->page;
+          $total = count($wishlist);
+          $target = ($currentPage - 1) * $perPage;
+          $total_pages = ceil($total / $perPage);
+        @endphp
+
+        @for ($i = 0; $i < $perPage; $i++)
+          @if (($target + $i) < $total)
+            @php
+              $product = \App\Models\Product::find($wishlist[$target + $i][0]);
+            @endphp
+            <div class="mb-3 flex flex-col items-center justify-between rounded bg-white px-4 py-5 shadow sm:flex-row sm:py-4">
+              <a href="/product/{{ $product->id }}" class="flex w-full flex-col border-b border-grey-dark pb-4 text-center sm:w-1/3 sm:border-b-0 sm:pb-0 sm:text-left md:w-2/5 md:flex-row md:items-center">
+                <span class="font-hkbold block pb-2 text-center text-sm uppercase text-secondary sm:hidden">상품명</span>
+                <div class="relative mx-auto w-20 sm:mx-0 sm:mr-3 sm:pr-0">
+                  <div class="aspect-w-1 aspect-h-1 w-full">
+                    <img src="{{ asset('storage/'.$product->images->pluck('image')->first()) }}" alt="product image" class="object-cover"/>
+                  </div>
+                </div>
+                <span class="mt-2 font-hk text-base text-secondary">{{ $product->name }}</span>
+              </a>
+              <div class="w-full pb-4 text-center sm:w-1/6 sm:pr-6 sm:pb-0 sm:text-right xl:w-1/5 xl:pr-16">
+                <span class="font-hkbold block pt-3 pb-2 text-center text-sm uppercase text-secondary sm:hidden">가격</span>
+                <span class="font-hk text-secondary">{{ number_format($product->selling_price) }} 원</span>
               </div>
+              <a href="javascript:addTo('cart', {{ $product->id }}, 1);" class="btn btn-primary whitespace-nowrap">장바구니에 담기</a>
+              <i class="bx bx-x mr-6 cursor-pointer text-2xl text-grey-darkest sm:text-3xl" onclick="deleteFromWishlist({{ $product->id }});"></i>
             </div>
-            <span class="mt-2 font-hk text-base text-secondary">Classic Beige</span>
-          </div>
-          <div class="w-full border-b border-grey-dark pb-4 text-center sm:w-1/6 sm:border-b-0 sm:pb-0 md:w-1/6 xl:w-1/5">
-            <span class="font-hkbold block pt-3 pb-2 text-center text-sm uppercase text-secondary sm:hidden">수량</span>
-            <span class="font-hk text-secondary">11</span>
-          </div>
-          <div class="w-full pb-4 text-center sm:w-1/6 sm:pr-6 sm:pb-0 sm:text-right xl:w-1/5 xl:pr-16">
-            <span class="font-hkbold block pt-3 pb-2 text-center text-sm uppercase text-secondary sm:hidden">가격</span>
-            <span class="font-hk text-secondary">$1045</span>
-          </div>
-          <a href="/" class="btn btn-primary whitespace-nowrap">Order Now</a>
-        </div>
-        
-        <div class="mb-3 flex flex-col items-center justify-between rounded bg-white px-4 py-5 shadow sm:flex-row sm:py-4">
-          <div class="flex w-full flex-col border-b border-grey-dark pb-4 text-center sm:w-1/3 sm:border-b-0 sm:pb-0 sm:text-left md:w-2/5 md:flex-row md:items-center">
-            <span class="font-hkbold block pb-2 text-center text-sm uppercase text-secondary sm:hidden">상품명</span>
-            <div class="relative mx-auto w-20 sm:mx-0 sm:mr-3 sm:pr-0">
-              <div class="aspect-w-1 aspect-h-1 w-full">
-                <img
-                  src="https://elyssi.redpixelthemes.com/assets/img/unlicensed/backpack-3.png"
-                  alt="product image"
-                  class="object-cover"/>
-              </div>
-            </div>
-            <span class="mt-2 font-hk text-base text-secondary">Party Blake</span>
-          </div>
-          <div class="w-full border-b border-grey-dark pb-4 text-center sm:w-1/6 sm:border-b-0 sm:pb-0 md:w-1/6 xl:w-1/5">
-            <span class="font-hkbold block pt-3 pb-2 text-center text-sm uppercase text-secondary sm:hidden">수량</span>
-            <span class="font-hk text-secondary">10</span>
-          </div>
-          <div class="w-full pb-4 text-center sm:w-1/6 sm:pr-6 sm:pb-0 sm:text-right xl:w-1/5 xl:pr-16">
-            <span class="font-hkbold block pt-3 pb-2 text-center text-sm uppercase text-secondary sm:hidden">가격</span>
-            <span class="font-hk text-secondary">$1045</span>
-          </div>
-          <a href="/" class="btn btn-primary whitespace-nowrap">Order Now</a>
-        </div>
-        
-        
+          @endif
+        @endfor
+      
         <!-- pagination -->
         <div class="flex justify-center pt-6 md:justify-end">
           <span class="cursor-pointer pr-5 font-nanumgothic font-semibold text-grey-darkest transition-colors hover:text-black">이전</span>
-          <span class="mr-3 flex h-6 w-6 cursor-pointer items-center justify-center rounded-full font-gowunbatang text-sm font-semibold text-black transition-colors hover:bg-primary hover:text-white">1</span>
-          <span class="mr-3 flex h-6 w-6 cursor-pointer items-center justify-center rounded-full font-gowunbatang text-sm font-semibold text-black transition-colors hover:bg-primary hover:text-white">2</span>
-          <span class="mr-3 flex h-6 w-6 cursor-pointer items-center justify-center rounded-full font-gowunbatang text-sm font-semibold text-black transition-colors hover:bg-primary hover:text-white">3</span>
+          @for ($i = 1; $i <= $total_pages; $i++)
+          <a href="?page={{ $i }}" target="_self">
+            <span class="mr-3 flex h-6 w-6 cursor-pointer items-center justify-center rounded-full font-gowunbatang text-sm font-semibold text-black transition-colors hover:bg-primary hover:text-white">{{ $i }}</span>
+          </a>
+          @endfor
           <span class="cursor-pointer pl-2 font-nanumgothic font-semibold text-grey-darkest transition-colors hover:text-black">다음</span>
         </div>
         <!-- pagination End -->
-
+        
       </div>
     </div>
     <!-- 메인 컨텐츠 End -->
