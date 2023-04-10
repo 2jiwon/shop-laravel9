955ae8fee2f665d4997b366fedd4dfed26ebe78e
2jiwon <ljwjulian@gmail.com>
2023-04-10 21:26:13 +0900

    문의를 위한 Question 모델과 컨트롤러 추가

---
+++ app/Http/Controllers/QuestionsController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Question;
+use Illuminate\Http\Request;
+
+class QuestionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        $msg = "";
+        try {
+            $res = Question::create([
+                'product_id' => $request->product_id,
+                'user_id' => $request->user()->id,
+                'status' => 1,
+                'title' => $request->title,
+                'contents' => $request->contents,
+            ]);
+
+            if (!$res) throw new Exception("저장에 실패했어요.", 1);
+            
+        } catch (\Throwable $th) {
+            $msg = "fail ".$th->getMessage();
+        }
+
+        return response()->json(['result' => $msg]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Question $question)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Question $question)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Question $question)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Question $question)
+    {
+        //
+    }
+}
